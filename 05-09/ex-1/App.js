c599f36755310a9ae107014b15f7cf7393f18395
Diego Lopes da Silva <diego.silva.09lopes@gmail.com>
2025-09-05 19:44:37 -0300

    Fazendo a maioria dos exercicios de reposição de aula

---
+++ 05-09/ex-1/App.js
@@ -0,0 +1,6 @@
+const MeuArray = require("./MeuArray");
+
+const meuArray = new MeuArray();
+
+// Nessa ocasião, uma constante para armazenar o valor do novo array é criada, mas não exibe nada tanto pela falta de dados entregues
+// quanto pela falta de pedir ao sistema que demonstre, através de um console log ou table.
